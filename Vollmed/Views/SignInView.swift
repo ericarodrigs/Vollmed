f91cfe5f565a1140f31c2d750c973385d7cdeae1
Erica Rodrigues <ericamrodrigs@gmail.com>
2025-10-04 15:24:14 -0300

    feat: create SignIn and SignUp views

---
+++ Vollmed/Views/SignInView.swift
@@ -0,0 +1,76 @@
+//
+//  SignInView.swift
+//  Vollmed
+//
+//  Created by Erica Rodrigues on 04/10/25.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+
+    @State private var email: String = ""
+    @State private var password: String = ""
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16.0) {
+            Image(.logo)
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: 36.0, alignment: .center)
+
+            Text("Olá")
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.accent)
+
+            Text("Preencha para acessar sua conta")
+                .font(.title3)
+                .foregroundStyle(.gray)
+                .padding(.bottom)
+
+            Text("Email")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.accent)
+
+            TextField("Insira seu email", text: $email)
+                .padding(14.0)
+                .background(Color.gray.opacity(0.25))
+                .cornerRadius(14.0)
+                .autocorrectionDisabled()
+                .keyboardType(.emailAddress)
+                .textInputAutocapitalization(.never)
+
+            Text("Senha")
+                .font(.title3)
+                .bold()
+                .foregroundStyle(.accent)
+
+            SecureField("Insira sua senha", text: $password)
+                .padding(14.0)
+                .background(Color.gray.opacity(0.25))
+                .cornerRadius(14.0)
+            
+            Button (action: {
+                //
+            }, label: {
+                ButtonView(text: "Entrar")
+            })
+            
+            NavigationLink {
+                SignUpView()
+            } label: {
+                Text("Ainda não possui conta? Cadastre-se.")
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+        }
+        .padding()
+        .navigationBarBackButtonHidden()
+    }
+}
+
+#Preview {
+    SignInView()
+}
---
+++ Vollmed/Views/SignUpView.swift
@@ -0,0 +1,137 @@
+//
+//  SignUpView.swift
+//  Vollmed
+//
+//  Created by Erica Rodrigues on 04/10/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var cpf: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var healthPlan: String
+    @State private var password: String = ""
+    
+    let healthPlans: [String] = [
+        "Amil", "Unimed", "Bradesco", "SulAmerica", "Hapvida", "Notredame", "São Francisco", "Outro"
+    ]
+    
+    init() {
+        self.healthPlan = healthPlans[0]
+    }
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            VStackLayout(alignment: .leading, spacing: 16.0) {
+                Image(.logo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: 36.0, alignment: .center)
+                    .padding(.vertical)
+
+                Text("Olá, boas-vindas!")
+                    .font(.title2)
+                    .bold()
+                    .foregroundStyle(.accent)
+
+                Text("Insira seus dados para criar uma conta")
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                    .padding(.bottom)
+                
+                Text("Nome")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                 
+                TextField("Insira seu nome completo", text: $name)
+                    .padding(14.0)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .autocorrectionDisabled()
+                
+                Text("Email")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                 
+                TextField("Insira seu email", text: $email)
+                    .padding(14.0)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .autocorrectionDisabled()
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.never )
+                
+                Text("CPF")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                 
+                TextField("Insira seu CPF", text: $cpf)
+                    .padding(14.0)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .autocorrectionDisabled()
+                    .keyboardType(.numberPad)
+                
+                Text("Telefone")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                 
+                TextField("Insira seu telefone", text: $phoneNumber)
+                    .padding(14.0)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                    .keyboardType(.numberPad)
+                
+                Text("Senha")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                 
+                SecureField("Insira sua senha", text: $password)
+                    .padding(14.0)
+                    .background(Color.gray.opacity(0.25))
+                    .cornerRadius(14.0)
+                
+                Text("Selecione o seu plano de saúde")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.accent)
+                
+                Picker("Plano de saúde", selection: $healthPlan) {
+                    ForEach(healthPlans, id: \.self) {healthPlan in
+                        Text(healthPlan)
+                    }
+                }
+                
+                Button (action: {
+                    //
+                }, label: {
+                    ButtonView(text: "Cadastrar")
+                })
+                
+                NavigationLink {
+                    SignInView()
+                } label: {
+                    Text("Já possui conta? Faça o login!")
+                        .bold()
+                        .foregroundStyle(.accent)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+            }
+        }
+        .navigationBarBackButtonHidden()
+        .padding()
+    }
+}
+
+#Preview {
+    SignUpView()
+}
